01772771ac9dde5ae7888bc6be46f207746ae2e3
Brian Clark <bclarkx2@gmail.com>
2018-02-21 20:31:54 -0500

    Add basic command line structure to mapping module

--- mapping.py
+++ mapping.py
@@ -5,20 +5,27 @@
 ###############################################################################
 
 import argparse
-from collections import namedtuple
+# from collections import namedtuple
 
 
 ###############################################################################
 # Constants                                                                   #
 ###############################################################################
 
+BLUEPRINT_TO_IMAGE = "bti"
+IMAGE_TO_BLUEPRINT = "itb"
+
+OPERATIONS = [
+    BLUEPRINT_TO_IMAGE,
+    IMAGE_TO_BLUEPRINT
+]
 
 ###############################################################################
 # Classes                                                                     #
 ###############################################################################
 
-ImageCoord = namedtuple("ImageCoord", ["x", "y"])
-BlueprintCoord = namedtuple("BlueprintCoord", ["u", "v"])
+# ImageCoord = namedtuple("ImageCoord", ["x", "y"])
+# BlueprintCoord = namedtuple("BlueprintCoord", ["u", "v"])
 
 
 ###############################################################################
@@ -39,9 +46,21 @@ def blueprint_to_image(blueprint_coord):
 # Helper functions                                                            #
 ###############################################################################
 
+def format_coord(coord):
+    return "{}\n{}".format(*coord)
+
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--arg")
+    parser.add_argument("op",
+                        choices=OPERATIONS,
+                        help="Mapping operation")
+    parser.add_argument("coord",
+                        type=float,
+                        nargs=2,
+                        help="coordinates to map")
+    parser.add_argument("manifest",
+                        help="job_manifest_file")
     return parser.parse_args()
 
 
@@ -52,6 +71,13 @@ def get_args():
 def main():
 
     args = get_args()
+
+    if args.op == BLUEPRINT_TO_IMAGE:
+        coord = blueprint_to_image(tuple(args.coord))
+    elif args.op == IMAGE_TO_BLUEPRINT:
+        coord = image_to_blueprint(tuple(args.coord))
+
+    print(format_coord(coord))
 
 
 if __name__ == '__main__':
